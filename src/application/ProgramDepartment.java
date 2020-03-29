b51db5b4cdca0548dca29e7bfb9af6bef0f2ce80
leonardo rodrigues ferreira <leonardorodrigues_f@hotmail.com>
2020-03-29 20:28:08 -0300

    Criação de da classe principal ProgramDepartment e da classe DepartmentDaoJDBC

---
+++ src/application/ProgramDepartment.java
@@ -0,0 +1,18 @@
+package application;
+
+import model.DAO.DaoFactory;
+import model.DAO.SellerDAO;
+import model.Department;
+import model.Seller;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class ProgramDepartment {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+    }
+}
---
+++ src/model/DAO/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.DAO.impl;
+
+import model.DAO.DepartmentDAO;
+import model.Department;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDAO {
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return null;
+    }
+}
